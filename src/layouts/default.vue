16051f94337a2eed3fc211774014ae6d0e136e5a
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-08-13 16:30:13 +0530

    FIX: home page images

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
 import AppFooter from '@/components/AppFooter.vue'
 import AppHeader from '@/components/AppHeader.vue'
-import { useRoute } from 'vue-router'
+
 const route = useRoute()
 </script>
 <template>
@@ -10,6 +12,16 @@ const route = useRoute()
     <v-main>
       <router-view />
     </v-main>
+    <div class="d-flex justify-center mb-12" style="z-index: 1">
+      <!-- <v-btn>helo</v-btn> -->
+      <iframe
+        src="//www.topcreativeformat.com/902df83ba8e546bcd7a1e3781cde1806/invoke.js"
+        height="60"
+        width="468"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
     <AppFooter v-if="route.path !== '/login'" />
   </v-app>
 </template>
